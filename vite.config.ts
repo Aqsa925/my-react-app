02bb22ff32843700cf74798b0b59171f3a1e1e1f
Aqsa925 <aqsasafder52@gmail.com>
2025-05-23 22:58:11 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/my-react-app/',  // <-- your repo name with slashes
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
+
